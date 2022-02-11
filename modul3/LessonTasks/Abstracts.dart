ed60823254ee484ca0495b0236c66ebdeff425c7
Komiljon Zokirov <62002994+komzakdroid@users.noreply.github.com>
2022-02-11 11:16:51 +0500

    Update README.md

--- modul3/LessonTasks/Abstracts.dart
+++ modul3/LessonTasks/Abstracts.dart
@@ -1,8 +1,19 @@
 void main() {
-  ThisClass
+  Child child = Child();
+  child.abstractMethod();
 }
 
+class Child extends Parent {
+  @override
+  void abstractMethod() {
+    print("Abstract Method!");
+  }
+}
 
-abstract class ThisClass{
+abstract class Parent {
+  void abstractMethod();
 
+  void simpleMethod() {
+    print("Method2");
+  }
 }
